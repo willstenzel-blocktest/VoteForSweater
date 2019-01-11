4a50712544eafff7fdfa426d4a9a98506be63aef
wstenzel <wills.stenzel@gmail.com>
2019-01-11 15:36:40 -0500

    updated tests

--- TestVoteForSweaterV3.sol
+++ TestVoteForSweaterV3.sol
@@ -5,13 +5,6 @@ import "./VoteForSweaterV3.sol";
 contract TestVoteForSweaterV3 is VoteForSweaterV3 {
 
   function test_sweaterVote() {
-    // Voting for an invalid option 'turtle neck'
-    assert(getVotes("turtle neck") == 0);
-    assert(checkAlreadyVoted(msg.sender) == false);
-    sweaterVote("turtle neck");
-    assert(getVotes("turtle neck") == 0);
-    assert(checkAlreadyVoted(msg.sender) == false);
-
     // Voting for a valid option 'blue'
     uint blueVotes = getVotes("blue");
     assert(checkAlreadyVoted(msg.sender) == false);
@@ -19,5 +12,12 @@ contract TestVoteForSweaterV3 is VoteForSweaterV3 {
     blueVotes += 1;
     assert(getVotes("blue") == blueVotes);
     assert(checkAlreadyVoted(msg.sender) == true);
+
+    // Voting for an invalid option 'turtle neck'
+    assert(getVotes("turtle neck") == 0);
+    assert(checkAlreadyVoted(msg.sender) == false);
+    sweaterVote("turtle neck");
+    assert(getVotes("turtle neck") == 0);
+    assert(checkAlreadyVoted(msg.sender) == false);
   }
 }
